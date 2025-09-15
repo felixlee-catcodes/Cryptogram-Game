65369a0d728f44e82b88739b0df23123b9898c68
C. Felix Lee <felixlee.music@gmail.com>
2025-09-14 21:51:38 -0400

    ignore unused signals

--- systems/EventHub/EventHub.gd
+++ systems/EventHub/EventHub.gd
@@ -1,6 +1,14 @@
 extends Node 
 
+@warning_ignore_start("unused_signal")
+
 var keys = KeyboardEvents.new()
+var cells = LetterCellEvents.new()
+
+
+class LetterCellEvents:
+	signal cell_focused(cell)
+
 
 class KeyboardEvents:
 	signal keyboard_input(key)
