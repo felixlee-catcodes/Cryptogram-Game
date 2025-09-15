36eff8dba501ca37c86c12ff48c3fedcc647037d
C. Felix Lee <felixlee.music@gmail.com>
2025-09-15 16:14:12 -0400

    add randomized cipher

--- tools/EncryptionService.gd
+++ tools/EncryptionService.gd
@@ -8,28 +8,66 @@ var regEx = RegEx.new()
 # This will be where various types of cipher algorithms are written; to be called by PuzzleManager
 func _ready():
 	regEx.compile("[A-Za-z]")
-	caesar_encrypt(quote)
-	
-func caesar_encrypt(quote) -> String:
+	shuffle_caesar_encrypt(quote)
+
+
+func basic_caesar_encrypt(text) -> String:
 	var encrypted_quote : String = ""
 	var shift_amt = randi_range(1, alphabet.size() - 1)
 	var cipher : Dictionary = {}
 	for letter in alphabet:
 		var ct = apply_shift(letter, shift_amt)
 		cipher[letter] = ct
-	Log.prn("cipher dict: ", cipher)
 
-	for char : String in quote:
-		var match = regEx.search(char)
+	for _char : String in text:
+		var match = regEx.search(_char)
 		if match:
-			char = char.to_upper()
-			var crypt_char = cipher[char]
+			_char = _char.to_upper()
+			var crypt_char = cipher[_char]
 			encrypted_quote += crypt_char
 		elif not match: 
-			encrypted_quote += char
-			
-	Log.pr("encrypted quote: ", encrypted_quote)
+			encrypted_quote += _char
+
 	return encrypted_quote
+
+
+func shuffle_caesar_encrypt(_quote) -> String:
+	var encrypted_text : String = ""
+	var cipher : Dictionary = {}
+	var unique_chars = count_unique(_quote)
+	var used_indices : Array = []
+	var pool = alphabet.duplicate(true)
+	
+	# BUILDING THE CIPHER:
+	for char in unique_chars:
+		var choice = char
+		while choice == char:
+			choice = pool.pop_at(randi_range(0, pool.size() - 1))
+		cipher[char] = choice
+	
+	#ENCRYPTING THE INPUT TEXT:	
+	for _char : String in quote:
+		var match = regEx.search(_char)
+		if match:
+			_char = _char.to_upper()
+			var crypt_char = cipher[_char]
+			encrypted_text += crypt_char
+		elif not match: 
+			encrypted_text += _char
+
+	#Log.pr("plaintext: ", quote, "\nencrypted text: ", encrypted_text)
+	Log.prn("cipher: ", cipher)
+	return encrypted_text
+
+
+func count_unique(text) -> Array:
+	var unique_chars : Array = []
+	for _char in text:
+		var match = regEx.search(_char)
+		if match and not _char.to_upper() in unique_chars:
+			unique_chars.append(_char.to_upper())
+	return unique_chars
+
 
 func apply_shift(letter, shift) -> String:
 	var letter_idx = alphabet.find(letter)
