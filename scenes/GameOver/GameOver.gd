f5d9d59bb37323cbfd97385bed65eada97315deb
C. Felix Lee <felixlee.music@gmail.com>
2025-09-18 15:45:06 -0400

    delete unused GameOver scene

--- scenes/GameOver/GameOver.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-@onready var header_ui = $UILayer/HeaderUI
-
-func _ready():
-	var ui_container : HBoxContainer = header_ui.ui_container
-	header_ui.timer_display.visible = false
-	ui_container.add_theme_constant_override("separation", 505)
