a60db13a321648345dd4dcc20e0f3b628774b14a
C. Felix Lee <felixlee.music@gmail.com>
2025-09-19 12:19:36 -0400

    create QuoteEntry resource

---
+++ resources/QuoteBook/QuoteEntry.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name QuoteEntry
+
+@export var text : String
+@export var author : String
+@export var date_added : String
+#@export var tags : Array[String] = []
+@export var solve_time : int
