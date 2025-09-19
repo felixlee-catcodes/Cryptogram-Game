10f5a1b1d1b657acd29422a2613250ff1e8b9516
C. Felix Lee <felixlee.music@gmail.com>
2025-09-19 12:23:59 -0400

    hook up new game button to reload main scene

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -1,18 +1,28 @@
 extends Node
-#@onready var word_unit = $EncryptedMessageDisplay/WordUnit
-@onready var quote_scene = %QuoteScene
+# MAIN
 @onready var word_unit = preload("res://scenes/WordUnit/WordUnit.tscn")
 @onready var game_manager = $GameManager
-
-var test_sentence : String = "This is just a test."
+@onready var quote_scene = %QuoteScene
 
 func _ready():
+	EventHub.game.new_game.connect(_on_new_game)
+	setup_puzzle()
+	game_manager.start_game()
+
+
+func setup_puzzle():
 	var puzzle = game_manager.get_new_puzzle()
+	#Log.pr("new puzzle? ",puzzle)
 	var cipher_text = split_text(puzzle.cipherText)
 	quote_scene.word_array = cipher_text
 	quote_scene.author = puzzle.author
+	quote_scene.quote = puzzle.plainText
 	quote_scene.compile_quote()
-	game_manager.start_game()
+
+
+func _on_new_game():
+	get_tree().reload_current_scene()
+
 
 func split_text(quote: String) -> Array:
 	return quote.split(" ")
