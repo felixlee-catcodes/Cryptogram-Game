acba1296625573053ae81dd7544f59a0acdef60e
C. Felix Lee <felixlee.music@gmail.com>
2025-10-04 23:32:15 -0400

    reduce sfx volume

--- systems/SoundManager/SoundManager.gd
+++ systems/SoundManager/SoundManager.gd
@@ -6,10 +6,11 @@ extends Node
 func play_typing():
 	var p = AudioStreamPlayer.new()
 	p.stream = typing_sfx_stream
+	
 	add_child(p)
 	
 	# vary pitch:
 	p.pitch_scale = randf_range(0.90, 1.10)
-	
+	p.volume_linear = 0.08
 	p.play()
 	p.finished.connect(func(): p.queue_free())
