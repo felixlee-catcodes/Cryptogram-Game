5da15cc3344032b6a0bfff8d842d5826f0830f69
C. Felix Lee <felixlee.music@gmail.com>
2025-10-09 19:53:07 -0400

    remove unused helper scene+script

--- scenes/Helpers/KeyboardResponder/KeyboardResponder.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-func handle_notification(target: Control, what):
-	match what:
-		NOTIFICATION_VIRTUAL_KEYBOARD_SHOW:
-			var height = DisplayServer.virtual_keyboard_get_height()
-	
-		NOTIFICATION_VIRTUAL_KEYBOARD_HIDE:
-			pass
