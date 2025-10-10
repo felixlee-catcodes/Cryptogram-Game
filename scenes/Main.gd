e8adc6c77f77923d3a71cabfd086fb98d13a6da7
C. Felix Lee <felixlee.music@gmail.com>
2025-10-09 22:02:18 -0400

    add 2nd animation, fix missing or changed keyframe values on each animation

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -14,19 +14,26 @@ extends Node
 @export var transition_texture : Texture2D
 @export var keyboard_panel_color : Color
 
+var transition_animations : Array = ["Transition1", "Transition2"]
+
 func _ready():
 	ThemeManager.connect("theme_changed", Callable(self, "_on_theme_changed"))
 	if ThemeManager.active_theme != null:
 		_on_theme_changed(ThemeManager.active_theme)
 	set_panel_styling()
 	game_over_display.visible = false
-	#header_ui.visible = false
 	EventHub.game.new_game.connect(_on_new_game)
 	EventHub.game.game_over.connect(_on_game_over)
 	setup_puzzle()
-	$TransitionAnim.play("Transition1")
+	play_transition_anim()
+
+
+func play_transition_anim() -> void:
+	var rand_idx = randi_range(0, transition_animations.size() - 1)
+	var rand_anim = transition_animations[rand_idx]
+	Log.pr("anim name: ", rand_anim)
+	$TransitionAnim.play(rand_anim)
 	$TransitionAnim.animation_finished
-	
 
 
 func set_panel_styling() -> void:
@@ -35,6 +42,7 @@ func set_panel_styling() -> void:
 	keyboard_panel_container.add_theme_stylebox_override("panel", style)
 	texture_rect.set_texture(bg_image_texture)
 	transition_image.set_texture(transition_texture)
+
 
 func _on_theme_changed(theme: ColorTheme):
 	bg_image_texture = theme.bg_texture
@@ -54,7 +62,6 @@ func setup_puzzle():
 
 func _on_new_game():
 	get_tree().reload_current_scene()
-	Log.pr("signal recieved")
 
 
 func _on_game_over(_time, puzzle):
